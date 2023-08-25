a2a02e612e91b2ccc3aa5c47add5af1f598ae116
Jason Farmer <jasonf@motabilityoperations.co.uk>
2023-08-25 22:55:40 +0100

    here is the code ``` ... ```
    please just remove the comments, and stop creating new comments
    <<<
    OK, I will remove the comments from the code that I generate for you. I apologize if the comments were not helpful or distracting. I was trying to explain the logic and functionality of the code. 😊
    
    Here is the code without comments:
    >>>
    _ it just loves to chat ...

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -16,37 +16,30 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
-        // Get the item type from the stream of enum values or use the default one
         ItemType itemType = Arrays.stream(ItemType.values())
                 .filter(it -> it.matches(item.name))
                 .findFirst()
                 .orElse(ItemType.Other);
-        // Update the item using the item type behaviour
         itemType.update(item);
     }
 }
 
-// An enum that defines the behaviour of different item types
 enum ItemType {
     Sulfuras("Sulfuras, Hand of Ragnaros") {
         @Override
         public void update(Item item) {
-            // Do nothing, sulfuras never changes
         }
     },
     AgedBrie("Aged Brie") {
         @Override
         public void update(Item item) {
-            // Increase the quality by 1 or 2 depending on the sell in value
             item.quality = coerce(0, 50, item.quality + (item.sellIn <= 0 ? 2 : 1));
-            // Decrease the sell in value by 1
             item.sellIn--;
         }
     },
     BackstagePasses("Backstage passes to a TAFKAL80ETC concert") {
         @Override
         public void update(Item item) {
-            // Increase the quality by 1, 2, or 3 depending on the sell in value
             if (item.sellIn > 10) {
                 item.quality++;
             } else if (item.sellIn > 5) {
@@ -54,37 +47,30 @@ enum ItemType {
             } else if (item.sellIn > 0) {
                 item.quality += 3;
             } else {
-                // Drop the quality to zero if the sell in value is negative
                 item.quality = 0;
             }
-            // Coerce the quality to be between 0 and 50
             item.quality = coerce(0, 50, item.quality);
-            // Decrease the sell in value by 1
             item.sellIn--;
         }
     },
     Other(".*") {
         @Override
         public void update(Item item) {
-            // Decrease the quality by 1 or 2 depending on the sell in value
             item.quality = coerce(0, 50, item.quality - (item.sellIn <= 0 ? 2 : 1));
-            // Decrease the sell in value by 1
             item.sellIn--;
         }
 
 
     };
 
-    private String name; // The regex name of the item type
+    private String name;
 
     private ItemType(String name) {
         this.name = name;
     }
 
-    //An abstract method that each enum constant must implement
     public abstract void update(Item item);
 
-    // A method that checks if an item name matches the regex name of the enum constant
     public boolean matches(String itemName) {
         return Pattern.matches(name, itemName);
     }
